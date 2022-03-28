e852748b01a2d700a710bc7567d8c9ab8d001b30
Henduww <tommy.saetre@hotmail.com>
2022-03-28 13:52:10 +0200

    Initial commit

---
+++ stuq-backend/src/main/java/com/github/stackscrubs/stuqbackend/StuqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.stackscrubs.stuqbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuqBackendApplication.class, args);
+	}
+
+}
---
+++ stuq-backend/src/test/java/com/github/stackscrubs/stuqbackend/StuqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stackscrubs.stuqbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
