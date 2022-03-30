a7c3e6f140d279daca5a025a2ae468a2d9819472
Henduww <tommy.saetre@hotmail.com>
2022-03-30 13:51:29 +0200

    Created BaseSelect componenet and wrote tests for it

---
+++ frontend/src/components/BaseSelect.vue
@@ -0,0 +1,37 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <select
+    v-bind="$attrs"
+    :value="modelValue"
+    @change="$emit('update:modelValue', $event.target.value)"
+    class="field"
+  >
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    >
+      {{ option }}
+    </option>
+  </select>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
