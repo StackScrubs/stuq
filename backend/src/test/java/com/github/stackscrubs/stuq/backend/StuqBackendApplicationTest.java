d96af0fabe21c70b17404cc0c6449a83bf76ef12
Henduww <tommy.saetre@hotmail.com>
2022-03-31 11:19:42 +0200

    Set up JPA tests to use in-memory DB

--- backend/src/test/java/com/github/stackscrubs/stuq/backend/StuqBackendApplicationTest.java
+++ backend/src/test/java/com/github/stackscrubs/stuq/backend/StuqBackendApplicationTest.java
@@ -1,4 +1,4 @@
-package com.github.stackscrubs.stuqbackend;
+package com.github.stackscrubs.stuq.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- backend/src/test/java/com/github/stackscrubs/stuq/backend/repository/AssignmentRepositoryTest.java
+++ backend/src/test/java/com/github/stackscrubs/stuq/backend/repository/AssignmentRepositoryTest.java
@@ -1,13 +1,13 @@
-package com.github.stackscrubs.stuqbackend.repository;
+package com.github.stackscrubs.stuq.backend.repository;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.github.stackscrubs.stuq.backend.StuqBackendApplication;
-import com.github.stackscrubs.stuq.backend.repository.AssignmentRepository;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ContextConfiguration(classes = StuqBackendApplication.class)
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AssignmentRepositoryTest {
     @Autowired
     private AssignmentRepository assignmentRepository;
