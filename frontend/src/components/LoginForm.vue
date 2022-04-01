7b5c77bd096cb4fa4e90ef521f27d5ff6e646288
Magnus Steensland <mag.steensland@gmail.com>
2022-04-01 10:40:36 +0200

    Fixed input error handling for login

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,30 +1,34 @@
 <template>
-  <form class="box" action="" v-on="submit">
+  <form class="box" @submit.prevent="onSubmit">
     <BaseInput
       id="email"
       label="Email"
       v-model="email"
       type="email"
-      class="login-form-item base-input-field"
+      class="base-input-field"
       placeholder="ola@nordman.no"
       data-testid="email-input"
+      :error="errors.email"
     />
     <BaseInput
       label="Password"
       v-model="password"
       type="password"
-      class="login-form-item base-input-field"
+      class="base-input-field"
       data-testid="password-input"
+      :error="errors.password"
     />
-    <button v-on:click="submit" class="login-form-item submit-button" data-testid="login-button">Submit</button>
+    <button type="submit" class="submit-button" data-testid="login-button">Submit</button>
     <label>{{ failedLoginMessage }}</label>
   </form>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { UserCredentials, InvalidCredentialsError } from "../types/UserCredentials";
+import { useField, useForm } from "vee-validate";
+import { object, string } from "yup"; 
 
+import { UserCredentials, InvalidCredentialsError } from "../types/UserCredentials";
 import store from "../store";
 import BaseInput from "./BaseInput.vue";
 
@@ -33,6 +37,10 @@ export default defineComponent({
     name: "LoginComponent",
     prop: {
         failedLoginMessage: "",
+        errors: {
+            email: "",
+            password: ""
+        }
     },
     data: (): {credentials: UserCredentials, failedLoginMessage: string } => {
         return {
@@ -41,20 +49,52 @@ export default defineComponent({
         };
     },
     methods: {
-        async submit() {
-            const email = this.credentials.email;
-            const password = this.credentials.password;
-            if (email && password) {
+        async onSubmit() {
+            await this.submit().then(async (userCredentials) => {
+                if (userCredentials === undefined || !(userCredentials.email && userCredentials.password)) {
+                    return
+                }
+                const email = userCredentials.email;
+                const password = userCredentials.password;
                 try {
                     await store.dispatch("login", { email, password });
-                //TODO: If OK => Route to home/queues-page (not created yet)
+                    //TODO: If OK => Route to home/queues-page (not created yet)
                 } catch (e) {
                     if (e instanceof InvalidCredentialsError) {
                         this.failedLoginMessage = "Ugyldig brukernavn eller passord";
                     }
                 }
-            }
+            })
         },
+    },
+    setup() {
+        const validationSchema = object({
+            email: string().email("invalid email").required("email is required"),
+            password: string().required("password is required")
+        });
+
+        const { handleSubmit, errors } = useForm({
+            validationSchema,
+            initialValues: {
+                email: "",
+                password: "",
+            },
+        });
+
+        const { value: email } = useField("email");
+        const { value: password } = useField("password");
+
+        const submit =  handleSubmit((values) => {
+            return values;
+        });
+
+        return {
+            validationSchema,
+            errors,
+            email,
+            password,
+            submit,
+        }
     },
 });
 </script>
@@ -80,16 +120,30 @@ export default defineComponent({
   font-size: 16px;
 }
 
+.submit-button:hover {
+  background-color: #5656ff;
+  cursor: pointer;
+}
+
 :deep(.base-input-field) {
   margin: 10px;
   padding: 10px 5px;
 }
 
-:deep(.login-form-item) {
-  margin: 0px 0px 20px 0px;
-}
 :deep(label) {
+  margin: 10px 0px 0px 0px;
   align-self: flex-start;
   text-align: left;
+}
+
+@media screen and (min-width: 600px) {
+    .box {
+      width: 400px;
+      max-width: 400px;
+    }
+
+    :deep(.base-input-field) {
+      width: 400px
+    }
 }
 </style>
