dcce8d8077fb9ce420d18f09e39b65c31b250cf3
Henduww <tommy.saetre@hotmail.com>
2022-04-04 19:25:51 +0200

    Wrote JavaDoc

--- backend/src/main/java/com/github/stackscrubs/stuq/backend/service/QueueService.java
+++ backend/src/main/java/com/github/stackscrubs/stuq/backend/service/QueueService.java
@@ -19,6 +19,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * QueueService handles student queues for specific subjects,
+ * allowing for the creation and deletion of such queues,
+ * as well as adding and removing students from these queues.
+ * 
+ * The handling of queues is done purely in-memory and all
+ * operations must therefore be thread-safe.
+ */
 @Service
 public class QueueService {
 
@@ -32,16 +40,36 @@ public class QueueService {
 
     Logger logger = LoggerFactory.getLogger(QueueService.class);
 
+    /**
+     * Gets all active queues.
+     * @return List of all active queues.
+     */
     public List<Queue> getActiveQueues() {
         return this.activeQueues;
     }
 
+    /**
+     * Gets the queue for a subject with a given subject code that takes place during the given term.
+     * Thread-safe as the list of active queues are solely stored in memory.
+     * @param termId The ID of the term in which the subject takes place.
+     * @param subjectCodeThe subject's code.
+     * @return Queue for the subject.
+     * @throws QueueNotFoundException The subject has no active queue.
+     */
     public synchronized Queue getBySubject(TermId termId, String subjectCode) {
         logger.debug("Finding queue");
 
         return this.findQueueOrThrow(termId, subjectCode).clone();
     }
 
+    /**
+     * Adds a student to a subject's queue.
+     * @param termId The ID of the term in which the subject takes place.
+     * @param subjectCodeThe subject's code.
+     * @param studentId The ID of the student to add to queue.
+     * @throws QueueNotFoundException The subject has no active queue.
+     * @throws StudentNotFoundException The student with the given ID was not found.
+     */
     public synchronized void addStudentToQueue(TermId termId, String subjectCode, int studentId) {
         Queue queue = this.findQueueOrThrow(termId, subjectCode);
         Student student = this.studentService.getStudent(studentId);
@@ -49,6 +77,12 @@ public class QueueService {
         queue.addStudent(new QueueStudent(student));
     }
 
+    /**
+     * Removes a student from a queue.
+     * @param studentId The student to remove from the queue.
+     * @throws StudentNotFoundException The student with the given ID was not found.
+     * @throws StudentNotFoundInQueueException The student is not currently in a queue.
+     */
     public synchronized void removeStudentFromQueue(int studentId) {
         Student student = this.studentService.getStudent(studentId);
 
@@ -63,6 +97,14 @@ public class QueueService {
         queue.removeStudent(new QueueStudent(student));
     }
 
+    /**
+     * Creates a new active queue for the given subject.
+     * @param termId The ID of the term in which the subject takes place.
+     * @param subjectCodeThe subject's code.
+     * @throws QueueAlreadyExistsException The queue is already active.
+     * @throws TermNotFoundException The term with the given ID is not registered in the database.
+     * @throws SubjectNotFoundException The subject code is not registered in the database.
+     */
     public synchronized void createBySubject(TermId termId, String subjectCode) {
         logger.debug("Creating queue");
         
@@ -82,6 +124,14 @@ public class QueueService {
         this.activeQueues.add(queue);
     }
 
+    /**
+     * Deletes a queue from the list of active queues given its subject.
+     * @param termId The ID of the term in which the subject takes place.
+     * @param subjectCodeThe subject's code.
+     * @throws QueueNotFoundException The queue is not currently active.
+     * @throws TermNotFoundException The term with the given ID is not registered in the database.
+     * @throws SubjectNotFoundException The subject code is not registered in the database.
+     */
     public synchronized void deleteBySubject(TermId termId, String subjectCode) {
         logger.debug("Deleting queue");
 
@@ -100,6 +150,13 @@ public class QueueService {
         logger.debug("Deleted queue");
     }
 
+    /**
+     * Helper method for finding an active queue given its subject.
+     * @param termId The ID of the term in which the subject takes place.
+     * @param subjectCodeThe subject's code.
+     * @return The active queue of the given subject.
+     * @throws QueueNotFoundException The queue is not currently active.
+     */
     private synchronized Queue findQueueOrThrow(TermId termId, String subjectCode) {
         return this.activeQueues.stream()
             .filter((queue) ->
