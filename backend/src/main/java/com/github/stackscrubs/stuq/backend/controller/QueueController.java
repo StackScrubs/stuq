ce7c6d195a720ae467af1e84285dc819013a00c2
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-04-04 16:58:12 +0200

    Added swagger dependency, configuration and allowed swagger to be rec… (#22)
    * Added swagger dependency, configuration and allowed swagger to be reched without auth
    
    * Removed commented out dependencies

--- backend/src/main/java/com/github/stackscrubs/stuq/backend/controller/QueueController.java
+++ backend/src/main/java/com/github/stackscrubs/stuq/backend/controller/QueueController.java
@@ -10,7 +10,6 @@ import com.github.stackscrubs.stuq.backend.service.QueueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
