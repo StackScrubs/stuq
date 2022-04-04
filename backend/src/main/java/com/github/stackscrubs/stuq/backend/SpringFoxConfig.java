9df0e7e781168c86bc407a0398cc6b337cbc1def
Henduww <tommy.saetre@hotmail.com>
2022-04-04 22:15:45 +0200

    Fixed problems caused by springfox

--- backend/src/main/java/com/github/stackscrubs/stuq/backend/SpringFoxConfig.java
+++ backend/src/main/java/com/github/stackscrubs/stuq/backend/SpringFoxConfig.java
@@ -1,33 +1,34 @@
-package com.github.stackscrubs.stuq.backend;
+// package com.github.stackscrubs.stuq.backend;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+// import springfox.documentation.builders.PathSelectors;
+// import springfox.documentation.builders.RequestHandlerSelectors;
+// import springfox.documentation.spi.DocumentationType;
+// import springfox.documentation.spring.web.plugins.Docket;
+// import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * Spring configuration class.
- * Used to enable and configure Swagger
- * and generation of API documentation.
- */
-@EnableSwagger2
-@Configuration
-public class SpringFoxConfig {     
+// /**
+//  * Spring configuration class.
+//  * Used to enable and configure Swagger
+//  * and generation of API documentation.
+//  */
 
-    /**
-     * Bean used by Swagger to configure generation of API docs.
-     * @return Configured Docket object used by Swagger.
-     */
-    @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.basePackage("com.github.stackscrubs.stuq.backend.controller"))              
-          .paths(PathSelectors.any())
-          .build();                     
-    }
-}
+// @EnableSwagger2
+// @Configuration
+// public class SpringFoxConfig {     
+
+//     /**
+//      * Bean used by Swagger to configure generation of API docs.
+//      * @return Configured Docket object used by Swagger.
+//      */
+//     @Bean
+//     public Docket api() { 
+//         return new Docket(DocumentationType.SWAGGER_2)
+//           .select()
+//           .apis(RequestHandlerSelectors.basePackage("com.github.stackscrubs.stuq.backend.controller"))              
+//           .paths(PathSelectors.any())
+//           .build();                     
+//     }
+// }
