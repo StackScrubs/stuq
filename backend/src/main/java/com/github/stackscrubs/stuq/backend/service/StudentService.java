40e6261f182d47121ff2d3e0d773157ab216e8f7
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-04-04 19:32:27 +0200

    Feat/queue endpoint (#23)
    * Made use of authentication to automatically infer student that is to be queued
    
    * Wrote JavaDoc

--- backend/src/main/java/com/github/stackscrubs/stuq/backend/service/StudentService.java
+++ backend/src/main/java/com/github/stackscrubs/stuq/backend/service/StudentService.java
@@ -9,6 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * StudentService handles operations on students stored in the database.
+ */
 @Service
 public class StudentService {
     
@@ -17,10 +20,22 @@ public class StudentService {
 
     Logger logger = LoggerFactory.getLogger(StudentService.class);
 
+    /**
+     * Gets a student by a given ID.
+     * @param id The ID of the student to get.
+     * @return The student with the given ID.
+     * @throws StudentNotFoundException There is no student with the given ID.
+     */
     public Student getStudent(int id) {
         return getStudentOrThrow(id);
     }
 
+    /**
+     * Helper method for finding a student in the database or throwing an exception if it's not found.
+     * @param id The ID of the student to get.
+     * @return The student with the given ID.
+     * @throws StudentNotFoundException There is no student with the given ID.
+     */
     private Student getStudentOrThrow(int id) {
         return this.studentRepository.findById(id)
             .orElseThrow(() -> {
