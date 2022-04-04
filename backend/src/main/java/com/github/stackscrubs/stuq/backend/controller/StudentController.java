461cb5ddfe3473a30e8f20f8bbedf9ac08c5d9a7
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-04-04 14:01:01 +0200

    Feat/configure cors (#21)
    * Created CorsConfig
    
    * Added CORS env var to docker-compose configs

--- backend/src/main/java/com/github/stackscrubs/stuq/backend/controller/StudentController.java
+++ backend/src/main/java/com/github/stackscrubs/stuq/backend/controller/StudentController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/student")
 @EnableAutoConfiguration
-@CrossOrigin(origins = "") //TODO:
 public class StudentController {
     
     @Autowired
